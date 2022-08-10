6cdfe3e5eceea0b53c72d6b90b5795bcd7c7af47
RAJAONARIVELO Ravoatra Mihaja <mrajaonarivelo@klanik.com>
2022-08-10 17:18:00 +0400

    feat(#2): Adding color model

---
+++ models/hsl-color.ts
@@ -0,0 +1,16 @@
+import { Color } from "./color";
+
+interface HSLColorProperty {
+    hue: number;
+    saturation: number;
+    lightness: number;
+}
+
+export class HSLColor implements Color<HSLColorProperty> {
+    type: string = "hsl";
+    composition: HSLColorProperty = {
+        hue: 0,
+        saturation: 0,
+        lightness: 0,
+    };
+}
