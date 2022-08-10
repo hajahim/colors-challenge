91fb45b9ac3bbb0b9ba00f501413c836b28bfe2f
RAJAONARIVELO Ravoatra Mihaja <mrajaonarivelo@klanik.com>
2022-08-10 22:18:12 +0400

    doc(#3): Adding swagger for API Documentation

---
+++ pages/api-documentation.tsx
@@ -0,0 +1,30 @@
+import { GetStaticProps, InferGetStaticPropsType } from 'next';
+import { createSwaggerSpec } from 'next-swagger-doc';
+import dynamic from 'next/dynamic';
+import 'swagger-ui-react/swagger-ui.css';
+
+const SwaggerUI = dynamic(import('swagger-ui-react'), { ssr: false });
+
+function ApiDocumentation({ spec }: InferGetStaticPropsType<typeof getStaticProps>) {
+    return <SwaggerUI spec={spec} />;
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+    const spec: Record<string, any> = createSwaggerSpec({
+        definition: {
+            openapi: '3.0.0',
+            info: {
+                title: 'Colors API Endpoint',
+                version: '0.1.0',
+            },
+        },
+    });
+
+    return {
+        props: {
+            spec,
+        },
+    };
+};
+
+export default ApiDocumentation;
